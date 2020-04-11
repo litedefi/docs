891e68631e6aded29e0febbb2720241e3448ed9f
Oikon.cash <admin@oikon.cash>
2020-04-10 23:33:57 -0300

    unhexlify address for tronscan, remove unused text

--- scripts/build-addresses.js
+++ scripts/build-addresses.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const snx = require('@oikos/oikos');
 const moment = require('moment');
+const tronWeb = require('tronweb');
 
 const { version, date } = require('./utils');
 
@@ -18,7 +19,7 @@ const generateAddresses = () => {
 		date(),
 	).format('MMM D, YYYY')}\n\n`;
 
-	headerContent +=
+	/*headerContent +=
 		'!!! info "Naming Conventions"\n\tWithin the developer ecosystem, the names of Oikos contracts are referred to by their `source` Solidity file.' +
 		'If the same source is used for multiple instances of contracts with different constructor arguments - such as `Synth.sol`, `TokenState.sol` and `ProxyERC20.sol`' +
 		' - then the convention is to suffix the name of the differing feature to the end - be it the proxy target ' +
@@ -27,7 +28,7 @@ const generateAddresses = () => {
 	headerContent +=
 		'\n\n!!! tip "Handy Contract Links"\n\tAll of the below contracts can be accessed by our [contract linker](https://github.com/Synthetixio/contract-linker) utility. Simply suffix the contract name to the end of' +
 		' https://contracts.oikos.cash to get a link that will always redirect to the latest version of the contract on Tronscan.\n\n\tFor example, try https://contracts.oikos.cash/Oikos to get linked to the latest Oikos underlying.' +
-		'\n\n\tFor testnets, insert the testnet name before the contract, as in https://contracts.oikos.cash/kovan/Oikos';
+		'\n\n\tFor testnets, insert the testnet name before the contract, as in https://contracts.oikos.cash/kovan/Oikos';*/
 
 	const contractContent = [/*'mainnet',*/ 'shasta']
 		.map(network => {
@@ -41,13 +42,14 @@ const generateAddresses = () => {
 					.sort()
 					.map(targetContract => {
 						const { address, source } = targets[targetContract];
+						const addressReadable = tronWeb.address.fromHex(address);
 						const networkPrefix = network !== 'mainnet' ? network + '.' : '';
 						return `
               <tr>
                 <td>${targetContract}</td>
                 <td><a target="_blank" href="https://github.com/oikos-cash/oikos/blob/master/contracts/${source}.sol">${source}.sol</a></td>
                 <td><a target="_blank" href="https://raw.githubusercontent.com/oikos-cash/oikos-js/master/lib/abis/${network.toLowerCase()}/${source}.json">${source}.json</a></td>
-                <td><a target="_blank" href="https://${networkPrefix}tronscan.io/address/${address}">${address}</a>
+                <td><a target="_blank" href="https://${networkPrefix}tronscan.io/address/${addressReadable}">${addressReadable}</a>
                 </td>
               </tr>`;
 					})
